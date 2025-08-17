3d6c202c2a68a64ac87cb8f995343555df7f35c9
Chris Wong <chriswong.broadband@gmail.com>
2025-08-17 19:33:12 +0100

    Add signIn and signOut

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -3,6 +3,28 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    }
+    catch(error) {
+        if (error instanceof AuthError) {
+            switch(error.type) {
+                case 'CredentialsSignin':
+                    return 'invalid credentials';
+                default:
+                    return 'Something went wrong';
+            }
+        }
+        throw error;
+    }
+}
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl:'require'});
 
